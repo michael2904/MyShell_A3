eae9af9254e8b5d9ca3a5a612a7d83013facad85
Michael ABDALLAH-MINCIOTTI <michael.abdallah-minciotti@mail.mcgill.ca>
2014-11-15 23:10:43 -0500

    commented

--- MyShell.c
+++ MyShell.c
@@ -12,27 +12,34 @@
 #include <ctype.h>
 
 int main(int argc, const char * argv[]) {
+//initial prompt
     char *prompt = "Donde esta la bibliotheca";
-
+//while loop to do the new shell
     while(1){
+//print every time the prompt
         printf("%s --> ",prompt);
+//get char array for the entry and its lowercase version
         char entry[200];
         char lEntry[200];
+//get the entry
         fgets(entry,200,stdin);
-        char *temp=strtok(entry,"\n");
+//take out the new line
+	 char *temp=strtok(entry,"\n");
         int i;
-
+//turn to lowercase
         for(i = 0; entry[i]; i++){
             lEntry[i] = tolower(entry[i]);
         }
 
-        
+        //check if quit option and only quit (size==4)
         if(strncmp(lEntry,"quit",4)==0 && strlen(entry)==4){
             break;
         }
+//check if it is set prompt option
         else if(strncmp(lEntry,"set prompt ",11)==0 && strlen(entry)>11){
             prompt = (temp+11);
         }
+//else do as a normal shell
         else{
             system(entry);
         }
